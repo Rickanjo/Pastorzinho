5e0a9ab754978c8c8663ed653edeec9cad6b457c
Rickanjo <ricardo.cassettaro07@gmail.com>
2023-03-10 22:52:08 -0300

    Exercício Resolvido

--- Program.cs
+++ Program.cs
@@ -1,7 +1,52 @@
-﻿
+﻿//"Dó, ré, mi, fá, fá, fá,
 Console.WriteLine("Pastorzinho");
-Console.WriteLine("Dó, ré, mi, fá, fá, fá,");
-Console.WriteLine("Dó, ré, dó, ré, ré, ré,");
-Console.WriteLine("Dó, sol, fá, mi, mi, mi,");
-Console.WriteLine("Dó, ré, mi, fá, fá, fá.");
+Console.WriteLine();
+
+Console.WriteLine("Dó, "); Console.Beep(1320, 200);
+Console.WriteLine("ré, "); Console.Beep(1485, 200);
+Console.WriteLine("mi, "); Console.Beep(1650, 200);
+Console.WriteLine("fá, "); Console.Beep(1759, 200);
+Thread.Sleep(500);
+Console.WriteLine("fá, "); Console.Beep(1759, 200);
+Console.WriteLine("fá, "); Console.Beep(1759, 200);
+Thread.Sleep(1000);
+
+Console.WriteLine();
+
+//Dó, ré, dó, ré, ré, ré,
+
+Console.WriteLine("Dó, "); Console.Beep(1320, 200);
+Console.WriteLine("ré, "); Console.Beep(1485, 200);
+Console.WriteLine("dó, "); Console.Beep(1320, 200);
+Console.WriteLine("ré, "); Console.Beep(1485, 200);
+Thread.Sleep(1000);
+Console.WriteLine("ré, "); Console.Beep(1485, 200);
+Console.WriteLine("ré, "); Console.Beep(1485, 200);
+Thread.Sleep(1000);
+
+//Dó, sol, fá, mi, mi, mi,
+Console.WriteLine("Dó, "); Console.Beep(1320, 200);
+Console.WriteLine("sol, "); Console.Beep(1980, 200);
+Console.WriteLine("fá, "); Console.Beep(1759, 200);
+Console.WriteLine("mi, "); Console.Beep(1650, 200);
+Thread.Sleep(1000);
+Console.WriteLine("mi, ");Console.Beep(1650, 200);
+Console.WriteLine("mi, ");Console.Beep(1650, 200);
+Thread.Sleep(1000);  
+
+//Dó, ré, mi, fá, fá, fá.
+Console.WriteLine("Dó,"); Console.Beep(1320, 200);
+Console.WriteLine("ré, "); Console.Beep(1485, 200);
+Console.WriteLine("mi,"); Console.Beep(1650, 200);
+Console.WriteLine("fá"); Console.Beep(1759, 200);
+Thread.Sleep(1000);
+Console.WriteLine("fá, ");Console.Beep(1759, 200);
+Console.WriteLine("fá. ");Console.Beep(1759, 200);
+Thread.Sleep(1000);
+
+
+
+
+
+
 
